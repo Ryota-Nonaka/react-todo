e12883371916c1fc3daff725a89989630877e338
ノナカリョウタ <67694580+Ryota-Nonaka@users.noreply.github.com>
2021-06-05 12:45:32 +0900

    Initial commit

---
+++ src/components/IncompleteTodos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const IncompleteTodos = (props) => {
+  const { todos, onClickComplete, onClickDelete } = props;
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            //mapを使ってレンダリングしないと仮想DOMが差分を読み取る際に目印がないと
+            // 正確に読み取れない。そのためにkeyを設定する必要がある。
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
